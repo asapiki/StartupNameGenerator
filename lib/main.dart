0114c10204bda2e10fefc584d004b4f68d979b9c
asapiki <aiki.asap@gmail.com>
2020-04-01 17:18:24 +0900

    パスワードを隠すとリファクタ少々

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:startup_namer/models/user_model.dart';
+import 'package:startup_namer/pages/login_page.dart';
 import 'package:startup_namer/pages/random_words_page.dart';
 
 void main() {
@@ -25,82 +26,11 @@ class MyApp extends StatelessWidget {
       home: ListenableProvider<UserModel>(
         create: (_) => UserModel(),
         child: Consumer<UserModel>(
-          builder: (context, value, child) {
-            return value.isLoggedIn ? RandomWordsPage() : buildLoginPage(value);
+          builder: (context, userModel, child) {
+            return userModel.isLoggedIn
+                ? RandomWordsPage()
+                : LoginPage(userModel: userModel);
           },
-        ),
-      ),
-    );
-  }
-
-  Scaffold buildLoginPage(UserModel user) {
-    var emailController = TextEditingController();
-    var passwordController = TextEditingController();
-    const _showPassword = true;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              child: Column(
-                children: [
-                  Container(
-                    width: 200,
-                    child: TextFormField(
-                      controller: emailController,
-                      decoration: const InputDecoration(
-                        icon: Icon(Icons.email),
-                        hintText: 'xxx@yyy.com',
-                        labelText: 'email',
-                      ),
-                    ),
-                  ),
-                  Container(
-                    width: 200,
-                    child: TextFormField(
-                      controller: passwordController,
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        icon: Icon(Icons.https),
-                        hintText: 'abcd1234',
-                        labelText: 'password',
-//                          suffixIcon: IconButton(
-//                            icon: Icon(_showPassword
-//                                ? Icons.remove_red_eye
-//                                : Icons.panorama_fish_eye),
-//                          ),
-                      ),
-                    ),
-                  ),
-                  RaisedButton(
-                    child: Text('email/password'),
-                    onPressed: () {
-                      user.signInWithEmail(
-                        emailController.value.text,
-                        passwordController.value.text,
-                      );
-                    },
-                  ),
-                ],
-              ),
-            ),
-            RaisedButton(
-              child: Text('google'),
-              onPressed: () {
-                user.signInWithGoogle();
-              },
-            ),
-            RaisedButton(
-              child: Text('github'),
-              onPressed: () {
-                user.signInWithGithub();
-              },
-            ),
-          ],
         ),
       ),
     );
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,20 +1,103 @@
-//import 'package:firebase_auth/firebase_auth.dart';
-//import 'package:flutter/material.dart';
-//
-//class LoginPage extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      body:  RaisedButton(
-//        child: Text('login'),
-//        onPressed: () {
-//          _handleSignIn().then((FirebaseUser user) {
-//            setState(() {
-//              this.user = user;
-//            });
-//          }).catchError((e) => print(e));
-//        },
-//      ),
-//    );
-//  }
-//}
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  final userModel;
+
+  const LoginPage({Key key, this.userModel}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  var _hidePassword = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _biuldEmailAuth(),
+            _buildGoogleAuth(),
+            _buildGithubAuth(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  RaisedButton _buildGithubAuth() {
+    return RaisedButton(
+      child: Text('github'),
+      onPressed: () {
+        widget.userModel.signInWithGithub();
+      },
+    );
+  }
+
+  RaisedButton _buildGoogleAuth() {
+    return RaisedButton(
+      child: Text('google'),
+      onPressed: () {
+        widget.userModel.signInWithGoogle();
+      },
+    );
+  }
+
+  Column _biuldEmailAuth() {
+    return Column(
+      children: [
+        Container(
+          width: 200,
+          child: TextFormField(
+            controller: emailController,
+            decoration: const InputDecoration(
+              icon: Icon(Icons.email),
+              hintText: 'xxx@yyy.com',
+              labelText: 'email',
+            ),
+          ),
+        ),
+        Container(
+          width: 200,
+          child: TextFormField(
+            controller: passwordController,
+            obscureText: _hidePassword,
+            decoration: InputDecoration(
+              icon: Icon(Icons.https),
+              hintText: 'abcd1234',
+              labelText: 'password',
+              suffixIcon: IconButton(
+                icon: Icon(_hidePassword
+                    ? Icons.remove_red_eye
+                    : Icons.panorama_fish_eye),
+                onPressed: () {
+                  setState(() {
+                    _hidePassword = !_hidePassword;
+                  });
+                },
+              ),
+            ),
+          ),
+        ),
+        RaisedButton(
+          child: Text('email/password'),
+          onPressed: () {
+            widget.userModel.signInWithEmail(
+              emailController.value.text,
+              passwordController.value.text,
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
